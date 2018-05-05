f68695a066610c2780281ae322f042fd9029483f
DeathStarGnR <nsbailey2010@gmail.com>
2018-05-05 18:00:22 -0400

    Inital commit

---
+++ public/staff/pages/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>GBI</title>
+  <meta name="author" content="name">
+  <meta name="description" content="description here">
+  <meta name="keywords" content="keywords,here">
+  <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+  <link rel="stylesheet" href="//fonts.googleapis.com/css?family=font1|font2|etc" type="text/css">
+  <link rel="stylesheet" href="stylesheet.css" type="text/css">
+  </head>
+  <body>
+    <h1>GBI Staff Area</h1>
+    <navigation>
+        <ul>
+            <li><a href="index.php">Menu</a></li>
+        </ul>
+    </navigation>
+    <footer>
+        &copy; <?php echo date('Y')?> Globe Bank
+    </footer>
+  </body>
+</html>
