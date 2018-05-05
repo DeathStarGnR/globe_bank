c23eec04e8c5d601cf110f4a209378a9e45ecb1f
DeathStarGnR <nsbailey2010@gmail.com>
2018-05-05 18:52:47 -0400

    Almost end of lesson 1

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -8,18 +8,15 @@
   <meta name="description" content="description here">
   <meta name="keywords" content="keywords,here">
   <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
-  <link rel="stylesheet" href="//fonts.googleapis.com/css?family=font1|font2|etc" type="text/css" />
+  <link rel="stylesheet" href="//fonts.googleapis.com/css?family=font1|font2|etc" type="text/css">
   <link rel="stylesheet" media="all" href="../stylesheets/staff.css" type="text/css" />
   </head>
   <body>
-    <h1>GBI Staff Area</h1>
+    <header>  
+        <h1>GBI Staff Area</h1>
+    </header>
     <navigation>
         <ul>
             <li><a href="index.php">Menu</a></li>
         </ul>
     </navigation>
-    <footer>
-        &copy; <?php echo date('Y')?> Globe Bank
-    </footer>
-  </body>
-</html>
--- public/staff/index.php
+++ public/staff/index.php
@@ -1,25 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>GBI</title>
-  <meta name="author" content="name">
-  <meta name="description" content="description here">
-  <meta name="keywords" content="keywords,here">
-  <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
-  <link rel="stylesheet" href="//fonts.googleapis.com/css?family=font1|font2|etc" type="text/css">
-  <link rel="stylesheet" href="stylesheet.css" type="text/css">
-  </head>
-  <body>
-    <h1>GBI Staff Area</h1>
-    <navigation>
-        <ul>
-            <li><a href="index.php">Menu</a></li>
-        </ul>
-    </navigation>
-    <footer>
-        &copy; <?php echo date('Y')?> Globe Bank
-    </footer>
-  </body>
-</html>
+<?php require_once('../../private/initialize.php');?>
+
+<?php include('../../private/shared/staff_header.php'); ?>
+
+<div id="content">
+</div>
+
+<?php include('../../private/shared/staff_footer.php'); ?>
